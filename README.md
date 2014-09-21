60c98da46bf7aa65e3df8dc24ed170abfc37898e
PTChan Extension <ptchanextension@gmail.com>
2014-09-21 01:12:53 +0100

    Initial Import

---
+++ README.md
@@ -0,0 +1,32 @@
+PTChan-Chrome-Extension
+=======================
+
+Segure o xã, amarre o xã, segure o xã xã xã xã xã
+
+Como usar
+=====================
+1. Esta extensão é para **o browser** Google Chrome ![Lida com isso](http://i.imgur.com/hJNpb7h.png)
+2. Fazer download da extensão [aqui](https://github.com/ptchanextension/PTChan-Chrome-Extension/raw/master/PTChan-Chrome-Extension.crx) (ficheiro "PTChan-Chrome-Extension.crx" na lista acima).
+3. Para instalar a extensão:
+ 1. Abrir a pasta onde está a extensão
+ 2. Abrir a página de extensões do chrome (colocar chrome://extensions na barra de endereço)
+ 3. Arrastar o ficheiro PTChan-Chrome-Extension.crx da pasta para o browser
+ 4. Clicar em "Adicionar"/"Add" no alerta que aparece
+ 5. ????
+ 6. Profiteroles
+
+Funcionalidades
+=====================
+Videos WebM
+---------------------
+É só fazer upload do ficheiro .webm como se fosse outro qualquer. Vai aparecer uma barra a indicar o progresso do upload.
+
+![WebM upload](http://i.imgur.com/MkH9GUg.png)
+
+Clicar na imagem ou no link acima da imagem abre a WebM.
+
+![WebM post](http://i.imgur.com/oOip3lk.png)
+
+Sugestões
+---------------------
+Sugere mais funcionalidades através das [Issues](https://github.com/ptchanextension/PTChan-Chrome-Extension/issues) ou no PTChan.
---
+++ data/contentscript.js
@@ -0,0 +1,200 @@
+/*
+ * This project was built using the spaghetti code paradigm
+ * with help of the ImInAHurrySoScrewIt framework.
+ */
+
+$(document).ready(function()
+{
+    handleExistingWebms();
+    handleWebmPost();
+});
+
+/*
+ * Searches for posts that have WebM videos uploded with this extension
+ * then changes de DOM accordingly.
+ */
+function handleExistingWebms()
+{
+    //Get all posts that contain the [webm] tag
+    var webms = $("blockquote div:contains([webmdata])");
+    
+    //Iterate through them
+    for(i=0; i<webms.length; i++)
+    {
+        //Get post text
+        var webmPostContents = $(webms[i]).text();
+        //Extract metadata from the post text
+        var webmURL = window.atob(webmPostContents.substring(webmPostContents.indexOf("[webmdata]")+10,webmPostContents.indexOf("[/webmdata]")));
+        //Get post's image link
+        var webmPostLink  = $(webms[i]).parent().parent().find("> a > span > img.thumb").parent().parent();
+        //Get post's filename link
+        var webmPostFilenameImageLink = webmPostLink.prev().prev().find("a");
+        
+        //Change the post's image link to the WebM URL
+        webmPostLink.attr("href", webmURL);
+        //Change the filename link contents to the WebM URL
+        webmPostFilenameImageLink.html(webmURL);
+        //Change the filename link href to the WebM URL
+        webmPostFilenameImageLink.attr("href", webmURL);
+        //Override the onclick handle for the filename link, to open the WebM in a new window
+        webmPostFilenameImageLink.attr("onclick", "window.open('"+webmURL+"'); return false;");
+        //Remove the first line from the post (contains metadata for this extension, no longer needed)
+        $(webms[i]).text($(webms[i]).text().split("\n").slice(1).join("\n"));
+    }
+}
+
+/*
+ * Handles a WebM upload through PTChan's post form
+ */
+function handleWebmPost()
+{
+    //Override form submit behaviour
+    $("#postform").submit(function(e)
+    {        
+        //Get the file input control
+        var fileInput = $(this).find("input[name='imagefile']")[0];
+        //If there's no file
+        if(typeof fileInput === "undefined" || fileInput.files.length === 0)
+        {
+            //Continue normally
+            return true;
+        }
+    
+        //Get the file to upload
+        var webmFile = fileInput.files[0];
+        //If it's a WebM file
+        if(webmFile.name.substr(-4) === "webm")
+        {
+            //Disable submit button to prevent multiple submitions
+            $(this).find("input[type='submit']").attr("disabled", "disabled");
+
+            //Prevent PTChan's form from POSTing normally while we're doing all this stuff
+            e.preventDefault();
+            
+            //Construct a form according to webmshare.com's upload form
+            var formData = new FormData();
+            //Append WebM file
+            formData.append("file", webmFile);
+            //Append a caption
+            formData.append("caption", "");
+            
+            //Add a progress bar to PTChan's post form
+            $(fileInput).parent().append('<progress id="webmprogress" max="100" value="0"></progress>');
+
+            //Upload WebM to webmshare.com through AJAX
+            $.ajax(
+            {
+                url: "http://webmshare.com/upload",
+                type: "POST",
+                data: formData,
+                cache: false,
+                processData: false,
+                contentType: false,
+                xhr: function()
+                {
+                    /*
+                     * Modify the default xhr object to update the progress of the upload
+                     */
+                    var myXhr = $.ajaxSettings.xhr();
+                    myXhr.upload.addEventListener("progress", function(event)
+                    {
+                        $("#webmprogress").attr("value", Math.ceil((event.loaded/event.total)*100));
+                    }, false);
+                
+                    return myXhr;
+                },
+                success: function(data)
+                {
+                    /*
+                     * Upload successfull
+                     */
+                    
+                    //Construct an HTML document from webmshare.com result data
+                    var resultDocument = $(data);
+                    //Extract WebM URL
+                    var directPlayURL = resultDocument.find("video source").attr("src");
+                    //Extract Thumbnail URL
+                    var thumbnailURL = $($(resultDocument.find(".form-control")[2]).text()).find("img").attr("src");
+                    
+                    /*
+                     * Prepare to make the PTChan post
+                     */
+                    //Clear the file from PTChan's post form
+                    $("#postform input[type='file']")[0].value = "";
+                    //Add metadata to post's header
+                    $("#postform textarea")[0].value = "[spoiler][webmdata]"+window.btoa(directPlayURL)+"[/webmdata] Para veres o vídeo instala a extensão. Chrome: https://github.com/ptchanextension/PTChan-Chrome-Extension/raw/master/PTChan-Chrome-Extension.crx Firefox: [/spoiler]\n"+$("#postform textarea")[0].value;
+                    //Construct a form according to PTChan's post form
+                    var ptChanFormData = new FormData($("#postform")[0]);
+                    
+                    /*
+                     * We need the thumbnail image as a blob in order to be
+                     * able to upload it to PTChan through the FormData object.
+                     * For this we need to use XMLHttpRequest directly (no jQuery)
+                     */
+                    //Construct XMLHttpRequest object
+                    var xhr = new XMLHttpRequest();
+                    //Handler for when the request finishes
+                    xhr.onreadystatechange = function()
+                    {
+                        //If all went well
+                        if (this.readyState === 4 && this.status === 200)
+                        {
+                            //Add image blob to PTChan's form
+                            ptChanFormData.append("imagefile", this.response, "thumbnail.jpg");
+                            
+                            //Original XMLHttpRequest holder
+                            var originalXhr;
+                            //Proceed to make a post to PTChan using AJAX
+                            $.ajax(
+                            {
+                                url: "http://www.ptchan.net/board.php",
+                                type: "POST",
+                                data: ptChanFormData,
+                                cache: false,
+                                processData: false,
+                                contentType: false,
+                                xhr: function()
+                                {
+                                    /*
+                                     * Save the default XMLHttpRequest object
+                                     */
+                                    originalXhr = $.ajaxSettings.xhr();
+                                    return originalXhr;
+                                },
+                                success: function(data)
+                                {
+                                    //Remove progress bar (this shouldn't be needed, but just in case)
+                                    $("#webmprogress").remove();
+                                    
+                                    //Post successful, redirect to destination
+                                    var destination = window.location.href;
+                                    //(This is a very recent attribute, so we test it)
+                                    if(typeof originalXhr.responseURL !== "undefined")
+                                    {
+                                        //(If we reach here we can handle noko)
+                                        destination = originalXhr.responseURL;
+                                    }
+                                    window.location = destination;
+                                },
+                                error: function(jqxhr, textStatus, errorThrown)
+                                {
+                                    //Remove progress bar
+                                    $("#webmprogress").remove();
+                                }
+                            });
+                        }
+                    };
+                    //Make the AJAX request
+                    xhr.open('GET', thumbnailURL);
+                    xhr.responseType = 'blob';
+                    xhr.send();
+                },
+                error: function(data)
+                {
+                    //Remove progress bar
+                    $("#webmprogress").remove();
+                }
+            });
+        }
+    });
+}
